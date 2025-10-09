Fix fuel tank mass calculation and mission variable handling
Refactored compute_fuel_tank_properties to use numpy for differentiation and corrected mass update logic. Updated add_mission_variables in converge.py to properly handle mission unknowns and input string construction for single point segments.

RCAIDE/Library/Methods/Powertrain/Sources/Fuel_Tanks/compute_fuel_tank_properties.py
@@ -8,13 +8,15 @@
 # ----------------------------------------------------------------------------------------------------------------------
 # RCAIDE imports
 import RCAIDE
+import numpy as np
+
 # ----------------------------------------------------------------------------------------------------------------------
 #  METHOD
 # ----------------------------------------------------------------------------------------------------------------------  
 def compute_fuel_tank_properties(tank,state,distributor,network_tag):
-    # '''
-    # SAI HEADER
-    # ''' 
+    '''
+    UPDATE HEADER 
+    ''' 
     
     if type(distributor) == RCAIDE.Library.Components.Powertrain.Distributors.Electrical_Bus:
         distributor_conditions = state.conditions.energy.busses[distributor.tag] 
@@ -22,19 +24,16 @@ def compute_fuel_tank_properties(tank,state,distributor,network_tag):
         distributor_conditions = state.conditions.energy.fuel_lines[distributor.tag]         
     
     tank_conditions = distributor_conditions.fuel_tanks[tank.tag]   
-    mass_unknowns   = state.unknowns.network[tank.tag + '_mass'][:,0]
-
-    t0  = state.numerics.time.control_points[0][0]
-    tf = state.numerics.time.control_points[-1][0]
-    D = state.numerics.dimensionless.differentiate
-    D_t = D/(tf - t0) 
-
-    tank_conditions.mass_flow_rate  =  distributor_conditions.fuel_mass_flow_rate * tank.flow_split_ratio
-    dm_l = -tank_conditions.mass_flow_rate[:, 0]
-    R = D_t @ mass_unknowns - dm_l
-    R[0] = mass_unknowns[0] - tank_conditions.mass[0, 0]
-
-    state.residuals.network[tank.tag + '_mass'] = R
-    tank_conditions.mass[1:, 0] = mass_unknowns[1:]
+    mass_unknowns   = state.unknowns.network[tank.tag + '_mass'] 
+     
+    D   = state.numerics.time.differentiate      
+    if len(D) > 0:
+        tank_conditions.mass_flow_rate  =  distributor_conditions.fuel_mass_flow_rate * tank.flow_split_ratio
+        dm_l                            = -tank_conditions.mass_flow_rate 
+        R                               = np.dot(D,mass_unknowns)[:, 0] - dm_l[:, 0]
+        R[0]                            = mass_unknowns[0] - tank_conditions.mass[0, 0]
+    
+        state.residuals.network[tank.tag + '_mass'] = R
+        tank_conditions.mass[1:, 0]                 = mass_unknowns[1:, 0]
 
     return 

RCAIDE/Library/Mission/Solver/converge.py
@@ -220,7 +220,7 @@ def add_mission_variables(segment):
         full_lower_bound_vals[unkn] = np.atleast_2d(segment.state.unknowns_lower_bounds.mission[unkn])
         full_upper_bound_vals[unkn] = np.atleast_2d(segment.state.unknowns_upper_bounds.mission[unkn])    
     
-    if segment.state.numerics.network_solver.method is None:
+    if segment.state.numerics.network_solver.method is None and (single_pt_seg != True):
         for unkn in net_unknown_keys: 
             full_unkn_vals[unkn]        = segment.state.unknowns.network[unkn]
             full_lower_bound_vals[unkn] = np.atleast_2d(segment.state.unknowns_lower_bounds.network[unkn])
@@ -295,13 +295,9 @@ def add_mission_variables(segment):
         
     elif single_pt_seg:  
         for unkn in unknown_keys:
-            basic_string_con[unkn] = np.tile('segment.state.unknowns.'+unkn+'[', n_points)
-            input_string.append(np.core.defchararray.add(basic_string_con[unkn],np.array([0]).astype(str))) 
-        if segment.state.numerics.network_solver.method is None:
-            for unkn in net_unknown_keys:  
-                basic_string_con[unkn] = np.tile('segment.state.unknowns.network.'+unkn+'[', n_points)
-                input_string_network.append(np.core.defchararray.add(basic_string_con[unkn],np.array([0]).astype(str)))
-            input_string = np.hstack((np.ravel(input_string),np.ravel(input_string_network)))  
+            basic_string_con[unkn] = np.tile('segment.state.unknowns.mission.'+unkn+'[', n_points)
+            input_string.append(np.core.defchararray.add(basic_string_con[unkn],np.array([0]).astype(str)))
+        input_string       = np.ravel(input_string)   
         input_string       = np.core.defchararray.add(input_string, np.tile(']',len_inputs))
         input_aliases      = np.reshape(np.tile(np.atleast_2d(np.array((None,None))),len_inputs), (-1, 2)) 
         input_aliases[:,0] = input_names